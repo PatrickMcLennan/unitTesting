62732ba6df0005cf399410e1bafa9805d0f25fd3
Patrick <patrick.a.mclennan@gmail.com>
2019-01-08 00:15:55 -0500

    testing

--- functions.test.js
+++ functions.test.js
@@ -11,18 +11,22 @@ const {
 test('Adds 2 + 2 to equal 4', () => {
   expect(add(2, 2)).toBe(4);
 });
+
 // to not be value
 test('Adds 2 + 2 to NOT equal 5', () => {
   expect(add(2, 2)).not.toBe(5);
 });
+
 //  test for null
 test('Should Be Null', () => {
   expect(isNull()).toBeNull();
 });
+
 // test for falsy
 test('Should be falsey', () => {
   expect(checkValue(null)).toBeFalsy();
 });
+
 // to be equal type & value
 test('User should be Patrick McLennan Object', () => {
   expect(createUser()).toEqual({
@@ -37,12 +41,32 @@ test('Should be under or equal 1600', () => {
   const load2 = 800;
   expect(load1 + load2).toBeLessThanOrEqual(1600);
 });
+
 //  Regex
 test('There is no I in team', () => {
   expect('team').not.toMatch(/I/i);
 });
+
 //  Arrays
 test('Admin should be in usernames', () => {
   const userNames = ['John', 'Karen', 'admin'];
   expect(userNames).toContain('admin');
+});
+
+// Working with async data
+
+Promise;
+test('User Fetched name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser()
+    .then((data) => {
+      expect(data.name).toEqual('Leanne Graham');
+    });
+});
+
+// Async
+test('User Fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
 });
