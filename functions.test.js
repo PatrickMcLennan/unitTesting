a404464b2ef05cf88377ccf7a707ce6c53d9bd5b
Patrick <patrick.a.mclennan@gmail.com>
2019-01-07 23:56:46 -0500

    testing

--- functions.test.js
+++ functions.test.js
@@ -6,6 +6,7 @@ const {
   checkValue,
   createUser,
 } = functions;
+
 // to be value
 test('Adds 2 + 2 to equal 4', () => {
   expect(add(2, 2)).toBe(4);
@@ -22,10 +23,26 @@ test('Should Be Null', () => {
 test('Should be falsey', () => {
   expect(checkValue(null)).toBeFalsy();
 });
-
+// to be equal type & value
 test('User should be Patrick McLennan Object', () => {
   expect(createUser()).toEqual({
     firstName: 'Pat',
     lastName: 'McLennan',
   });
+});
+
+//  less than or equal to numbers
+test('Should be under or equal 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+//  Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/i);
+});
+//  Arrays
+test('Admin should be in usernames', () => {
+  const userNames = ['John', 'Karen', 'admin'];
+  expect(userNames).toContain('admin');
 });
